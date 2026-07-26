0c6daf2e6cef3dd75ec3cf14bf6e267194b4f164
bwtschwarze-ux <bwt.schwarze@gmail.com>
2026-07-26 06:27:25 +0200

    Fischbilder: echte Fotos von Wikimedia Commons mit Attribution

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
-const CACHE = 'angelup-v8';
-const DATEIEN = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png',
-  './fische/karpfen.svg','./fische/hecht.svg','./fische/zander.svg','./fische/forelle.svg',
-  './fische/bachsaibling.svg','./fische/regenbogenforelle.svg','./fische/aal.svg','./fische/barsch.svg',
-  './fische/wels.svg','./fische/schleie.svg','./fische/rotauge.svg','./fische/rotfeder.svg',
-  './fische/brassen.svg','./fische/karausche.svg','./fische/barbe.svg','./fische/dorsch.svg',
-  './fische/hornhecht.svg'];
+const CACHE = 'angelup-v9';
+const DATEIEN = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(DATEIEN)).then(() => self.skipWaiting()));
 });
@@ -12,8 +7,11 @@ self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', e => {
+  // Wikimedia-Bilder nicht cachen (extern), damit sie live geladen werden
+  const u = new URL(e.request.url);
+  if (u.hostname.endsWith('wikimedia.org') || u.hostname.endsWith('wikipedia.org')) return;
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(netz => {
-    if (e.request.method === 'GET' && netz.ok && new URL(e.request.url).origin === location.origin) {
+    if (e.request.method === 'GET' && netz.ok && u.origin === location.origin) {
       const kopie = netz.clone();
       caches.open(CACHE).then(c => c.put(e.request, kopie));
     }
