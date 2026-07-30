73cd9952b730f2e903d55042870abfb95074f704
bwtschwarze-ux <bwt.schwarze@gmail.com>
2026-07-30 23:25:03 +0200

    Regionskarten: Titel unter Tag, Fisch weicher, Cache v19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'angelup-v18';
+const CACHE = 'angelup-v19';
 const DATEIEN = ['./', './index.html', './app.html', './lernen.html', './kurs.html',
   './stil.css', './impressum.html', './datenschutz.html',
   './manifest.webmanifest', './icon-192.png', './icon-512.png'];
