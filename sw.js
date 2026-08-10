611f41a2ca628d243a2d32c15b6735429ad6b526
bwtschwarze-ux <bwt.schwarze@gmail.com>
2026-08-10 21:12:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'angelup-v38';
+const CACHE = 'angelup-v39';
 const BILD_CACHE = 'angelup-bilder-v1';
 const DATEIEN = ['./', './index.html', './app.html', './lernen.html', './kurs.html',
   './live.html', './aal.html', './kurse.html', './vormerken.html',
